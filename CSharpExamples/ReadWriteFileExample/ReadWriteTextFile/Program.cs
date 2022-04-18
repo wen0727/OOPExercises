8adc9e3e4fb7a9a317558f66aec2d345b51451fb
wen2720 <wenhao2720@gmail.com>
2022-04-18 23:55:51 +0200

    Film Recommendation v1.2 Based on 3 requirements 1.1 Frequently viewd 1.2 Frequently purchased 2. Genre

--- CSharpExamples/ReadWriteFileExample/ReadWriteTextFile/Program.cs
+++ CSharpExamples/ReadWriteFileExample/ReadWriteTextFile/Program.cs
@@ -15,7 +15,7 @@ using CustomerClass;
 
 class Script {
     static void Main(string[] args) {
-        //Senario 1
+        //File -> Product list
         //Relative path
         string dataFile1 = @"data\Products.txt";
         
@@ -39,60 +39,66 @@ class Script {
         finally {
             //Close the file
             _SR.Close();
-            
             Console.WriteLine("File1 closed.");
         }
-
+        //Immutable Product list
         ImmutableList<Product> _products = _PRODUCTS.ToImmutableList();
         /*
-        foreach(Product p1 in _PRODUCTS) { 
-            Console.WriteLine(p1.ToString());
+        foreach (Product p in _products) { 
+            Console.WriteLine(p);
         }
-        Console.WriteLine(_P1.ToString());
         */
-
-        //1.1 Recommendation by rating.
-        /*
+        //Simple query on the Product list, which shows products in decending order
+        
         var _qReviewRank = from p in _products
                            orderby p.pReviewRank descending
                            select p;
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("An example shows the file input is successfully converted to modeled data.******************");
+        Console.WriteLine("");
         foreach (Product p in _qReviewRank) {
             Console.WriteLine(p.ToString());
         }
-        */
-        //Senario 1.2.1 Recommendation by prchused rate
-        //Senario 1.2.1 Recommendation by review(user views)
-        
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("The first example ends.**********************************************************************");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        //Requirement 1.1 Recommendation by prchused rate
         //Relative path
-        string dataFile3 = @"data\Users.txt";
+        string dataFile2 = @"data\Users.txt";
         
         //Pass file to StreamReader object
-        StreamReader _SR3 = new StreamReader(dataFile3);
-        string? _LINE3;   
+        StreamReader _SR2 = new StreamReader(dataFile2);
+        string? _LINE2;   
         List<Customer> _CS = new List<Customer>();
         //Read file untill reach the end of the file
         try {
             do {
-                _LINE3 = _SR3.ReadLine();
+                _LINE2 = _SR2.ReadLine();
                 //Console.WriteLine(_LINE1);
-                ContentReader _CR3 = new ContentReader(_LINE3);
-                Customer _CUSTOMER = new Customer(_CR3.pContent);
+                ContentReader _CR2 = new ContentReader(_LINE2);
+                Customer _CUSTOMER = new Customer(_CR2.pContent);
                 _CS.Add(_CUSTOMER);
-            } while (!_SR3.EndOfStream);
+            } while (!_SR2.EndOfStream);
         }
         catch (Exception e) {
-            Console.WriteLine("Exception3:" + e.Message);
+            Console.WriteLine("Exception2:" + e.Message);
         }
         finally {
             //Close the file
-            _SR3.Close();
-            Console.WriteLine("File3 closed.");
+            _SR2.Close();
+            Console.WriteLine("File2 closed.");
         }
         
         ImmutableList<Customer> _cs = _CS.ToImmutableList();
+        /*
         foreach (Customer c in _cs) { 
             Console.WriteLine(c);
         }
+        */
+
         //pid -> (pId*Count) List
         ImmutableList<(int,int)> fPidToPidCountPair(ImmutableList<int> pids, ImmutableList<(int,int)> acc) { 
             if (pids.IsEmpty) { 
@@ -102,7 +108,7 @@ class Script {
             }
         }
         
-        //fInsert: 
+        //fInsert: pid -> count -> (pid*count) list -> (pid*count) list -> (pid*count) list
         ImmutableList<(int,int)> fInsertMultiset(int pid, int count, ImmutableList<(int,int)> ms, ImmutableList<(int,int)> acc) { 
             if (ms.IsEmpty) {
                 return acc.Add((pid,count));
@@ -115,7 +121,7 @@ class Script {
             }
         }
         
-        /*
+        /*  Test for inserting element to multi set.
         var _TEST1 = fPidToPidCountPair(_cs[0].pViewedPids,ImmutableList<(int,int)>.Empty);
         foreach ((int,int) ms in _TEST1) { 
             Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
@@ -125,9 +131,8 @@ class Script {
             Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
         }
         */
-        //predicate, Exist                                  predicate whether a key exists
-
         
+        //pNegExist: pid -> (pid*count) -> bool -> bool     // predicates, a key not exist in a multi-set
         bool pNegExist(int pid, ImmutableList<(int,int)> ms, bool acc) {
             if (ms.IsEmpty) { 
                 return acc &= true;
@@ -135,7 +140,8 @@ class Script {
                 return  pNegExist(pid, ms.Remove(ms[0]), (acc &= ms[0].Item1 != pid));
             }
         }
-        //  (pId*Count) List -> (pId*Count) List -> (pId*Count) List   union of two multisets
+
+        //fUnionTwoMultiSets: (pId*Count) List -> (pId*Count) List -> (pId*Count) List   // union of two multi-sets
         ImmutableList<(int,int)> fUnionTwoMultiSets(ImmutableList<(int,int)> ms, ImmutableList<(int,int)> acc) { 
             if (ms.IsEmpty) { 
                 return acc;
@@ -148,29 +154,16 @@ class Script {
             }
         }
         
-        //Customer -> (pId*Count) List        create total multisets for all users
-        ImmutableList<(int,int)> fUsersMultisets(ImmutableList<Customer> cs, ImmutableList<(int,int)> acc) { 
+        //fUsersViewedMultisets:Customer -> (pId*Count) List        
+        ImmutableList<(int,int)> fUsersViewedMultisets(ImmutableList<Customer> cs, ImmutableList<(int,int)> acc) { 
             if (cs.IsEmpty) { 
                 return acc;
             } else { 
-                return fUsersMultisets(cs.Remove(cs[0]), fUnionTwoMultiSets(fPidToPidCountPair(cs[0].pViewedPids,ImmutableList<(int,int)>.Empty),acc));
+                return fUsersViewedMultisets(cs.Remove(cs[0]), fUnionTwoMultiSets(fPidToPidCountPair(cs[0].pViewedPids,ImmutableList<(int,int)>.Empty),acc));
             }
         }
 
-        var _qReconnByViewed = fUsersMultisets(_cs, ImmutableList<(int,int)>.Empty);
-        /*
-        foreach ((int,int) ms in _qReconnByViewed) { 
-           Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
-        }
-         */
-        var _qSortReconnByViewed = from ms in _qReconnByViewed
-                                   orderby ms.Item2 descending
-                                   select ms;
-        foreach ((int,int) ms in _qSortReconnByViewed) { 
-            Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
-        }
-
-        /*
+        /* Test for fUnionTwoMultiSets function
         var _TEST4 = fPidToPidCountPair(_cs[0].pViewedPids,ImmutableList<(int,int)>.Empty);
         foreach ((int,int) ms in _TEST4) { 
             Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
@@ -187,39 +180,89 @@ class Script {
         }
         */
 
+        //create a multisets of customer viewed products from all users 
+        var _qViewedPids = fUsersViewedMultisets(_cs, ImmutableList<(int,int)>.Empty);
+        /*
+        foreach ((int,int) ms in _qReconnByViewed) { 
+           Console.WriteLine(ms.Item1.ToString() + " " + ms.Item2.ToString());
+        }
+         */
+        var _qrecommendedbyViewRate = from ms in _qViewedPids
+                                      where ms.Item2 > 1
+                                      orderby ms.Item2 descending
+                                      join ps in _products on ms.Item1 equals ps.pID
+                                      select ps;
 
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Requiremtn 1.1******************************************************************");
+        Console.WriteLine("The frequently viewed films are listed as follows:");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        foreach (Product p in _qrecommendedbyViewRate){
+            Console.WriteLine(p.pName);
+        }
 
+        Console.WriteLine("");
+        Console.WriteLine("Solution for requirement 1.1 ends************************************************");
+        Console.WriteLine("");
+        
+        // Requirement 1.2
+        ImmutableList<(int,int)> fUsersPurchasedMultisets(ImmutableList<Customer> cs, ImmutableList<(int,int)> acc) { 
+            if (cs.IsEmpty) { 
+                return acc;
+            } else { 
+                return fUsersPurchasedMultisets(cs.Remove(cs[0]), fUnionTwoMultiSets(fPidToPidCountPair(cs[0].pPurchasedPids,ImmutableList<(int,int)>.Empty),acc));
+            }
+        }
+
+        var _qPurchasedPids = fUsersPurchasedMultisets(_cs, ImmutableList<(int,int)>.Empty);
+        var _qrecommendedbyPurchaseRate = from ms in _qPurchasedPids
+                                          where ms.Item2 > 1
+                                          orderby ms.Item2 descending
+                                          join ps in _products on ms.Item1 equals ps.pID
+                                          select ps;
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Requirement 1.2***************************************************************************************");
+        Console.WriteLine("The frequently sold films are listed as follows:");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        foreach (Product p in _qrecommendedbyPurchaseRate){
+            Console.WriteLine(p.pName);
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Solution for requirement 1.2 ends**************************************************************************");
+        Console.WriteLine("");
         
 
-        /*
-        ImmutableList<ImmutableDictionary<int,int>> fListofMultiset(ImmutableList<Customer> cs, ImmutableList<ImmutableDictionary<int,int>> acc) { 
-            
-        }
-        */
-        /*
-        //Senario 2
-        string dataFile2 = @"data\CurrentUserSession.txt";
-        StreamReader _SR2 = new StreamReader(dataFile2);
-        string? _LINE2;
+        //Requirement 2.
+        string dataFile3 = @"data\CurrentUserSession.txt";
+        StreamReader _SR3 = new StreamReader(dataFile3);
+        string? _LINE3;
         List<CurrentUserSession> _CURRENTUSERSESSION = new List<CurrentUserSession>();
         try {
             do {
-                _LINE2 = _SR2.ReadLine();
-                Console.WriteLine(_LINE2);
-                ContentReader _CR = new ContentReader(_LINE2);
-                CurrentUserSession _CUS = new CurrentUserSession(_CR.pContent);                  
+                _LINE3 = _SR3.ReadLine();
+                //Console.WriteLine(_LINE3);
+                ContentReader _CR3 = new ContentReader(_LINE3);
+                CurrentUserSession _CUS = new CurrentUserSession(_CR3.pContent);                  
                 _CURRENTUSERSESSION.Add(_CUS);
-            } while (!_SR2.EndOfStream);
+            } while (!_SR3.EndOfStream);
         }
         catch (Exception e) {
-            Console.WriteLine("Exception2:" + e.Message);
+            Console.WriteLine("Exception3:" + e.Message);
         }
         finally {
             //Close the file
-            _SR2.Close();
-            Console.WriteLine("File2 closed.");
+            _SR3.Close();
+            Console.WriteLine("File3 closed.");
         }
+        //CurrentUserSession list
         ImmutableList<CurrentUserSession> _cus = _CURRENTUSERSESSION.ToImmutableList();
+        //fExistsKW: string array -> string array -> bool   //predicates, key words matches
         bool fExistsKW(string[] kws1, string[] kws2) {
             bool EXISTS = false; 
             foreach (string kw in kws1) { 
@@ -232,6 +275,7 @@ class Script {
             return EXISTS;
         }
         
+        //fMatchProductByKeyWords: string array -> (fExistsKW: bool ) -> Product list -> Product list
         ImmutableList<Product> fMatchProductByKeyWords(string[] kws, ImmutableList<Product> ps, ImmutableList<Product> acc) { 
             if (ps.IsEmpty) { 
                 return acc;
@@ -243,6 +287,7 @@ class Script {
                 }
             }
         }
+        //fFindKeyWords : pid -> Product list -> string array
         string[] fFindKeyWords(int pid, ImmutableList<Product> ps) { 
             if(ps.IsEmpty) { 
                 return null;
@@ -254,6 +299,7 @@ class Script {
                 }    
             }
         }
+        //fFindProductBySession : CurrentUserSession list -> Product list -> (fMatchProductByKeyWords: Product list) -> Product list
         ImmutableList<Product> fFindProductBySession(ImmutableList<CurrentUserSession> cuss, ImmutableList<Product> ps, ImmutableList<Product> acc) { 
             if(cuss.IsEmpty) { 
                 return acc;
@@ -261,12 +307,20 @@ class Script {
                 return fFindProductBySession(cuss.Remove(cuss[0]), ps, fMatchProductByKeyWords(fFindKeyWords(cuss[0].pPid,ps),ps,acc));
             }
         }
-        var _qRecon = fFindProductBySession(_cus,_products,ImmutableList<Product>.Empty);
+        var _qProductsInSession = fFindProductBySession(_cus,_products,ImmutableList<Product>.Empty);
 
-        foreach(Product p in _qRecon) {
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Requirement 2************************************************************************");
+        Console.WriteLine("Each session's recommendation is listed as follows.");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        foreach(Product p in _qProductsInSession) {
             Console.WriteLine(p);
         }
-        */
+        Console.WriteLine("");
+        Console.WriteLine("Solution for requiment 2 ends**************************************************************");
+        
     }
 }
 
